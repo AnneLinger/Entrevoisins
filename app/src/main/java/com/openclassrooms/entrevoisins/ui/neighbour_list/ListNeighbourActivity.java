ed78eead79f7c8d7c54d95ff635007ea1bb46ba7
Anne Linger <anne-linger@orange.fr>
2021-10-28 08:49:41 +0200

    Initial commit

---
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/ListNeighbourActivity.java
@@ -0,0 +1,52 @@
+package com.openclassrooms.entrevoisins.ui.neighbour_list;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.widget.Button;
+
+import com.openclassrooms.entrevoisins.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class ListNeighbourActivity extends AppCompatActivity {
+
+    // UI Components
+    @BindView(R.id.tabs)
+    TabLayout mTabLayout;
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+    @BindView(R.id.container)
+    ViewPager mViewPager;
+
+    ListNeighbourPagerAdapter mPagerAdapter;
+    //TAG for lifecycle (logs)
+    private final String TAG = getClass().getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_neighbour);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(mToolbar);
+        mPagerAdapter = new ListNeighbourPagerAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(mPagerAdapter);
+        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabLayout));
+        mTabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
+
+        Log.d(TAG, "onCreate: ");
+
+    }
+
+    @OnClick(R.id.add_neighbour)
+    void addNeighbour() {
+        AddNeighbourActivity.navigate(this);
+        Log.d(TAG, "addNeighbour: ");
+    }
+}
